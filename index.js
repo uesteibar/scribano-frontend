efacac89d86a067dfe7e1fa5cd83384c4ccfafd3
uesteibar <uesteibar@gmail.com>
2019-04-02 18:33:07 +0200

    Use node-fetch

--- index.js
+++ index.js
@@ -1,61 +1,67 @@
 const http = require("http");
-const https = require("https");
+const fetch = require("node-fetch");
 const generator = require("asyncapi-generator");
 const fs = require("fs");
 const path = require("path");
-const URL = require('url').URL;
+const URL = require("url").URL;
 
-const specUrl = process.env.SPEC
+const specUrl = process.env.SPEC;
 
 const httpClient = () => {
-  const url = new URL(specUrl)
+  const url = new URL(specUrl);
   var client = http;
-  if (url.toString().indexOf("https") === 0){
-    return https
+  if (url.toString().indexOf("https") === 0) {
+    return https;
   }
 
-  return http
-}
+  return http;
+};
 
 const handleDocs = (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" }); // http header
 
   const file = fs.createWriteStream("asyncapi.json");
-  const request = httpClient().get(specUrl, function(
-    response
-  ) {
-    response.pipe(file);
-
-    generator
-      .generateTemplateFile({
-        template: "html",
-        file: "index.html",
-        config: {
-          asyncapi: path.resolve(__dirname, "asyncapi.json")
+  fetch(specUrl)
+    .then(res => res.text())
+    .then(body => {
+      fs.writeFile("asyncapi.json", body, function(err) {
+        if(err) {
+            return console.log(err);
         }
-      })
-      .then(result => {
-        res.write(result);
-        res.end();
-      })
-      .catch(console.error);
-  });
-}
+
+        console.log("The file was saved!");
+      });
+
+      generator
+        .generateTemplateFile({
+          template: "html",
+          file: "index.html",
+          config: {
+            asyncapi: path.resolve(__dirname, "asyncapi.json")
+          }
+        })
+        .then(result => {
+          res.write(result);
+          res.end();
+        })
+        .catch(console.error);
+    });
+};
 
 const handleCss = (req, res) => {
-  res.writeHead(200, {'Content-type' : 'text/css'});
-  const fileContents = fs.readFileSync('./css/main.css', {encoding: 'utf8'});
+  res.writeHead(200, { "Content-type": "text/css" });
+  const fileContents = fs.readFileSync("./css/main.css", { encoding: "utf8" });
   res.write(fileContents);
   res.end();
-}
+};
 
 http
   .createServer(function(req, res) {
     const url = req.url;
     if (url === "/") {
-      handleDocs(req, res)
+      handleDocs(req, res);
     } else if (url === "/css/main.css") {
-      handleCss(req, res)
+      handleCss(req, res);
     } else {
       res.writeHead(404, { "Content-Type": "text/html" }); // http header
       res.write("<h1>Not Found<h1>");
